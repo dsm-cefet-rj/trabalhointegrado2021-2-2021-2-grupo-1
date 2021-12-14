9bb3f45d6eee7e0b1d6cec157f5952b97eeb0010
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-14 18:50:10 -0300

    Update and rename reVenda.js to Venda.js

--- frontend/src/components/Venda.js
+++ frontend/src/components/Venda.js
@@ -1,9 +1,9 @@
 import "./Venda.css";
 
-function reVenda() {
+function Venda() {
   return (
     <>
-    <header>
+      <header>
         <div className="centralize-content header-wrapper">
           <h1 className="title">Meu Ingresso</h1>
           <div className="usuario-section">
@@ -30,38 +30,41 @@ function reVenda() {
           </ul>
         </nav>
       </header>
-    <main>
-        <h1 className="titulo">Cadastre Seus Ingressos para Revenda</h1>
+      <main>
+        <h1 className="titulo">Revenda seus Ingressos</h1>
         <h2 className="subtitulo">Preencha com as Informações Necessárias</h2>
-        <hr>
+        <hr/>
         <form className="checkout-form">
-            <p>Nome do Ingresso</p>
+            <p>Evento</p>
             <select className="primary-content-input">
                 <option>Fla x Flu (RJ)</option>
                 <option>Lolapalooza Dia 2 (SP)</option>
                 <option>Show Zeca Pagods (MG)</option>
                 <option>Teatro Estadual (ES)</option>
-            </select></p>
-            <hr>
+            </select>
+            <hr/>
             <p>Quantidade de Ingressos</p>
-            <input type="number" className="primary-content-input" placeholder="2">
-            <hr>
+            <p>
+              <input type="number" className="primary-content-input" placeholder="2"/>
+            </p>
+            <hr/>
             <p>Valor de Cada Ingresso</p>
             <p>
-            <input type="number" placeholder="R$ 100" className="primary-content-input">
+              <input type="number" placeholder="R$ 100" className="primary-content-input"/>
             </p>
-        </form><br>
+        </form>
+        <br/>
         <div className="submit container-botao">
           <button className = "botao">
             Cancelar
-        </button>
-        <button className = "botao">
+          </button>
+          <button className = "botao">
             Finalizar
-        </button>
+          </button>
         </div>
-    </main>
+      </main>
     </>
   );
 }
 
-export default reVenda;
+export default Venda;
