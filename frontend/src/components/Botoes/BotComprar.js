69f4ad225177abb92fae56f7023385f3bc35e82a
lucasmuguet <79877581+lucasmuguet@users.noreply.github.com>
2021-12-14 22:54:51 -0300

    Add files via upload

---
+++ frontend/src/components/Botoes/BotComprar.js
@@ -0,0 +1,13 @@
+import './botoes.css';
+
+function BotComprar(){
+  return(
+    <>
+    <div className="botoes-container">
+      <button className="botao-comprar">Comprar</button>
+    </div>
+    </>
+  );
+}
+
+export default BotComprar;
