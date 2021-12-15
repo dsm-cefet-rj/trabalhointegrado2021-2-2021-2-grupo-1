37b062bf3b1881f2a265d7306d676d5777c2a093
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-14 22:38:47 -0300

    Update CriarVenda.js

--- frontend/src/components/CriarVenda.js
+++ frontend/src/components/CriarVenda.js
@@ -10,43 +10,55 @@ function CriarVenda() {
       <header>
         <CabecalhoEmpresa />
         <MenuEmpresa />
-      </header>
-      <main>
-        <h1 className="titulo">Revenda seus Ingressos</h1>
-        <h2 className="subtitulo">Preencha com as Informações Necessárias</h2>
-        <hr/>
-        <form className="checkout-form">
-            <p>Evento</p>
-            <select className="primary-content-input">
-                <option>Fla x Flu (RJ)</option>
-                <option>Lolapalooza Dia 2 (SP)</option>
-                <option>Show Zeca Pagods (MG)</option>
-                <option>Teatro Estadual (ES)</option>
+        </header>
+      <main className="centralizar-xy centralizar-y">
+        <h2 className="subtitulo">Editar Venda</h2>
+        <form className="formulario">
+          <label>Nome do Evento</label>
+          <input type="text" className="input-box" placeholder="Rock in Rio" />
+          <label>
+            Genero
+            <select className="input-box">
+              <option>Esportes</option>
+              <option>Shows</option>
+              <option>Família</option>
             </select>
-            <hr/>
-            <p>Quantidade de Ingressos</p>
-            <p>
-              <input type="number" className="primary-content-input" placeholder="2"/>
-            </p>
-            <hr/>
-            <p>Valor de Cada Ingresso</p>
-            <p>
-              <input type="number" placeholder="R$ 100" className="primary-content-input"/>
-            </p>
+          </label>
+          <label>
+            Estado
+            <select className="input-box">
+              <option>RJ</option>
+              <option>SP</option>
+              <option>MG</option>
+              <option>ES</option>
+            </select>
+          </label>
+          <label>
+            Cidade
+            <input
+              type="text"
+              placeholder="Rio de Janeiro"
+              className="input-box"
+            />
+          </label>
+          <label>
+            CEP
+            <input type="number" placeholder="20270090" className="input-box" />
+          </label>
+          <label>
+            Valor do Ingresso
+            <input type="number" placeholder="R$ 100" className="primary-content-input"/>
+          </label>
+          <label>
+            Quantidade de Ingressos
+            <input type="number" placeholder="2" className="primary-content-input"/>
+          </label>
+          <EditarECriar />
         </form>
-        <br/>
-        <div className="submit container-botao">
-          <button className = "botao">
-            Cancelar
-          </button>
-          <button className = "botao">
-            Finalizar
-          </button>
-        </div>
-        <EditarECriar />
       </main>
     </>
   );
 }
+
 
 export default CriarVenda;
