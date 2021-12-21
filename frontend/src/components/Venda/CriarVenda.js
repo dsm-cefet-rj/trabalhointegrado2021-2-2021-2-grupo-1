febdc751bd09be3122e7c1e88dcb14a039b37f9a
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-21 20:41:27 -0300

    Update CriarVenda.js

--- frontend/src/components/Venda/CriarVenda.js
+++ frontend/src/components/Venda/CriarVenda.js
@@ -11,20 +11,20 @@ function CriarVenda() {
 
   return (
     <>
-      <Cabecalho usuario={"empresa"} />
-      <main className="centralizar-xy centralizar-y">
-        <h2 className="subtitulo">Criar Venda</h2>
-        <form className="formulario">
+      <Cabecalho usuario = {"empresa"} />
+      <main className = "centralizar-xy centralizar-y">
+        <h2 className = "subtitulo">Criar Venda</h2>
+        <form className = "formulario">
           <label>
             Nome do Evento
-            <select className="input-box">
+            <select className = "input-box">
               <option>Rock in Rio 2021</option>
               <option>Rock in Rio 2021</option>
             </select>
           </label>
           <label>
             Nome do Ingresso
-            <select className="input-box">
+            <select className = "input-box">
               <option>Rock in Rio 2021 - Dia 5</option>
               <option>Rock in Rio 2021 - Dia 7</option>
             </select>
@@ -37,7 +37,7 @@ function CriarVenda() {
             Quantidade de Ingressos
             <input type="number" placeholder="2" className="input-box" />
           </label>
-          <Botao botoes={botao} />
+          <Botao botoes = {botao}/>
         </form>
       </main>
     </>
