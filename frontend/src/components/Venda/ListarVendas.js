b047ab20b40f0254f8eecc85cb64f6262beb620a
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-21 15:37:42 -0300

    Update ListarVendas.js

--- frontend/src/components/Venda/ListarVendas.js
+++ frontend/src/components/Venda/ListarVendas.js
@@ -2,14 +2,20 @@ import Cabecalho from "../Cabecalho/Cabecalho";
 import Venda from "./Venda";
 
 function ListaVendas() {
+  const vendas = [
+    {nome: "Rock in Rio 2021",},
+    {nome: "Rock in Rio 2024",},
+  ];
+  
   return (
     <>
       <Cabecalho usuario={"empresa"} />
       <main className="centralizar-xy centralizar-y">
         <h2 className="subtitulo">Minhas Vendas</h2>
         <div className="listagem-container-empresa">
-          <Venda />
-          <Venda />
+          {vendas.map(({ nome }) => (
+            <Venda vendaNome={nome} />
+          ))}
         </div>
       </main>
     </>
