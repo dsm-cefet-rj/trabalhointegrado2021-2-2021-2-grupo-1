48b63c0c74957638855125b1b6e24ec9f2c04f7a
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2021-12-22 03:03:04 -0300

    Update ListarIngresso.js

--- frontend/src/components/Ingresso/Empresa/ListarIngresso.js
+++ frontend/src/components/Ingresso/Empresa/ListarIngresso.js
@@ -1,26 +1,15 @@
-import "../ingresso.css";
-
 import Cabecalho from "../../Cabecalho/Cabecalho";
 import Ingresso from "./Ingresso";
 
-function ListarIngressos() {
-  const ingresso = [
-    {
-      nome: "Rock in Rio 2022 - 3º Dia",
-    },
-    {
-      nome: "Rock in Rio 2022 - 4º Dia",
-    },
-  ];
-
+function ListarIngressos({ingressos}) {
   return (
     <>
-      <Cabecalho usuario={"empresa"} />
-      <main className="centralizar-xy centralizar-y">
-        <h2 className="subtitulo">Meus Ingressos</h2>
-        <div className="listagem-container-empresa">
-          {ingresso.map(({nome}) => (
-            <Ingresso ingressoNome = {nome} />
+      <Cabecalho usuario = {"empresa"} />
+      <main className = "centralizar-xy centralizar-y">
+        <h2 className = "subtitulo">Meus Ingressos</h2>
+        <div className = "listagem-container-empresa">
+          {ingressos.map(({nome}, i) => (
+            <Ingresso ingressoNome = {nome} key = {i} />
           ))}
         </div>
       </main>
