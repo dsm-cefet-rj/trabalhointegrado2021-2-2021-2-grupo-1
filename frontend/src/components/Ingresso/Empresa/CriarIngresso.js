38b21cf208286e0cbfdc1fe95f7a14994a4c8697
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2021-12-22 03:01:12 -0300

    Update CriarIngresso.js

--- frontend/src/components/Ingresso/Empresa/CriarIngresso.js
+++ frontend/src/components/Ingresso/Empresa/CriarIngresso.js
@@ -1,40 +1,65 @@
-import "../ingresso.css";
-
 import Cabecalho from "../../Cabecalho/Cabecalho";
-import Botao from "../../Botoes/Botoes";
+import "../../Botoes/botoes.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function CriarIngresso() {
-  const botao = [
-    {
-      nome: "Criar",
-      url: "/ingressos",
-    },
-  ];
+function CriarIngresso({ ingressos, setIngressos }) {
+  const navigate = useNavigate();
 
+  const [novoIngresso, setNovoIngresso] = useState({
+    evento: "",
+    nome: "",
+    dados: "",
+  });
+
+  function checaMudanca(e) {
+    setNovoIngresso({
+      ...novoIngresso,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function checaEnvio(e) {
+    setIngressos([...ingressos, novoIngresso]);
+    e.preventDefault();
+    navigate("/ingressos");
+  }
   return (
     <>
-      <Cabecalho usuario={"empresa"} />
-      <main className="centralizar-xy centralizar-y">
-        <h2 className="subtitulo">Criar Ingresso</h2>
-        <form className="formulario">
+      <Cabecalho usuario = {"empresa"} />
+      <main className = "centralizar-xy centralizar-y">
+        <h2 className = "subtitulo">Criar Ingresso</h2>
+        <form className = "formulario" onSubmit = {checaEnvio}>
+
           <label>Selecione um Evento</label>
-          <select className="input-box" placeholder="Evento">
-            <option>Evento1</option>
-            <option>Evento2</option>
+          <select name = "evento" className = "input-box"
+            placeholder = "Evento" onChange = {checaMudanca}>
+              <option>Evento1</option>
+              <option>Evento2</option>
           </select>
           <label>Nome do Ingresso</label>
           <input
-            type="text"
-            className="input-box"
-            placeholder="Rock in Rio - 3º Dia"
+            type = "text"
+            name = "nome"
+            className = "input-box"
+            placeholder = "Rock in Rio - 3º Dia"
+            onChange = {checaMudanca}
           />
           <label>Dados Adicionais</label>
           <input
-            type="text"
-            placeholder="Coloque alguma informação sobre o ingresso"
-            className="input-box"
+            type = "text"
+            name = "dados"
+            placeholder = "Coloque alguma informação sobre o ingresso"
+            className = "input-box"
+            onChange = {checaMudanca}
           />
-          <Botao botoes={botao} />
+          <div className = "botoes-container">
+            <input
+              type = "submit"
+              value = "Criar"
+              className = "botao botao-sucesso"
+            />
+          </div>
         </form>
       </main>
     </>
