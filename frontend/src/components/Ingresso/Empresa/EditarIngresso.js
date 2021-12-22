286e65e92d32a6292f9ced8d0244554d0b9cb2b7
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2021-12-22 03:01:52 -0300

    Update EditarIngresso.js

--- frontend/src/components/Ingresso/Empresa/EditarIngresso.js
+++ frontend/src/components/Ingresso/Empresa/EditarIngresso.js
@@ -17,28 +17,28 @@ function EditarIngresso() {
 
   return (
     <>
-      <Cabecalho usuario={"empresa"} />
-      <main className="centralizar-xy centralizar-y">
-        <h2 className="subtitulo">Editar Ingresso</h2>
-        <form className="formulario">
+      <Cabecalho usuario = {"empresa"} />
+      <main className = "centralizar-xy centralizar-y">
+        <h2 className = "subtitulo">Editar Ingresso</h2>
+        <form className = "formulario">
           <label>Selecione um Evento</label>
-          <select className="input-box" placeholder="Evento">
+          <select className = "input-box" placeholder = "Evento">
             <option>Evento1</option>
             <option>Evento2</option>
           </select>
           <label>Nome do Ingresso</label>
           <input
-            type="text"
-            className="input-box"
-            placeholder="Rock in Rio - 3º Dia"
+            type = "text"
+            className = "input-box"
+            placeholder = "Rock in Rio - 3º Dia"
           />
           <label>Dados Adicionais</label>
           <input
-            type="text"
-            placeholder="Coloque alguma informação sobre o ingresso"
-            className="input-box"
+            type = "text"
+            placeholder = "Coloque alguma informação sobre o ingresso"
+            className = "input-box"
           />
-          <Botoes botoes={botoes} />
+          <Botoes botoes = {botoes} />
         </form>
       </main>
     </>
