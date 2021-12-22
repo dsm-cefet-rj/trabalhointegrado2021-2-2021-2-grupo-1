c4c07c3cf4b8c3f3ba9e2e018e3cca3388b8ecb9
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-21 23:05:06 -0300

    Update CriarVenda.js

--- frontend/src/components/Venda/CriarVenda.js
+++ frontend/src/components/Venda/CriarVenda.js
@@ -1,13 +1,33 @@
 import Cabecalho from "../Cabecalho/Cabecalho";
 import Botao from "../Botoes/Botoes";
 
-function CriarVenda() {
-  const botao = [
-    {
-      nome: "Criar",
-      url: "/",
-    },
-  ];
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function CriarVenda({ vendas, setVendas }) {
+  const navigate = useNavigate();
+
+  const [novaVenda, setNovaVenda] = useState({
+    nomeEvento: "",
+    idIngresso: "";
+    genero: "",
+    valor: "",
+    quantidade: "",
+  });
+
+  function checaMudanca(e) {
+    setNovaVenda({
+      ...novaVenda,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function checaEnvio(e) {
+    setVendas([...vendas, novaVenda]);
+    e.preventDefault();
+    navigate("/vendas");
+  }
+
 
   return (
     <>
@@ -15,29 +35,46 @@ function CriarVenda() {
       <main className = "centralizar-xy centralizar-y">
         <h2 className = "subtitulo">Criar Venda</h2>
         <form className = "formulario">
+          <label>Nome do Evento</label>
+          <input
+            type="text"
+            name="nome"
+            className="input-box"
+            placeholder="Rock in Rio"
+            onChange={checaMudanca}
+          />
           <label>
-            Nome do Evento
-            <select className = "input-box">
-              <option>Rock in Rio 2021</option>
-              <option>Rock in Rio 2021</option>
-            </select>
-          </label>
-          <label>
-            Nome do Ingresso
+            Identificação do Ingresso
             <select className = "input-box">
               <option>Rock in Rio 2021 - Dia 5</option>
               <option>Rock in Rio 2021 - Dia 7</option>
             </select>
           </label>
           <label>
+            Genero
+            <select className="input-box" name="genero" onChange={checaMudanca}>
+              <option>Esportes</option>
+              <option>Shows</option>
+              <option>Família</option>
+            </select>
+          </label>
+          <label>
             Valor do Ingresso
-            <input type="number" placeholder="100" className="input-box" />
+              <input type="number" placeholder="100" className="input-box" />
+              onChange={checaMudanca}
           </label>
           <label>
             Quantidade de Ingressos
-            <input type="number" placeholder="2" className="input-box" />
+              <input type="number" placeholder="2" className="input-box" />
+              onChange={checaMudanca}
           </label>
-          <Botao botoes = {botao}/>
+          <div className="botoes-container">
+            <input
+              type="submit"
+              value="Criar"
+              className="botao botao-sucesso"
+            />
+          </div>
         </form>
       </main>
     </>
