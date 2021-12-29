9a2e9a531170dae12caf15f1a216f5775dfdf282
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2021-12-29 12:25:32 -0300

    Update ListarIngresso.js

--- frontend/src/components/Ingresso/Empresa/ListarIngresso.js
+++ frontend/src/components/Ingresso/Empresa/ListarIngresso.js
@@ -1,16 +1,22 @@
+import { useSelector } from "react-redux";
+
 import Cabecalho from "../../Cabecalho/Cabecalho";
 import Ingresso from "./Ingresso";
 
-function ListarIngressos({ingressos}) {
+function ListarIngressos() {
+  const ingressos = useSelector ((state) => state.ingressos);
+  
   return (
     <>
       <Cabecalho usuario = {"empresa"} />
       <main className = "centralizar-xy centralizar-y">
-        <h2 className = "subtitulo">Meus Ingressos</h2>
+        <h2 className = "subtitulo"> Meus Ingressos </h2>
         <div className = "listagem-container-empresa">
-          {ingressos.map(({nome}, i) => (
-            <Ingresso ingressoNome = {nome} key = {i} />
-          ))}
+          {ingressos.length > 0 ? (
+            ingressos.map((ingresso) => <Ingresso ingresso = {ingresso} key = {ingresso.id} />)
+          ) : (
+            <h2 className = "subtitulo"> Nenhum Ingresso Cadastrado </h2>
+          )}
         </div>
       </main>
     </>
