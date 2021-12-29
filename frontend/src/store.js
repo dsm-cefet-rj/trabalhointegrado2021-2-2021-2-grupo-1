6982daef3f431482fc2192df203f330e66d01819
lucasmuguet <79877581+lucasmuguet@users.noreply.github.com>
2021-12-28 22:27:39 -0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import eventosSlice from "./redux/eventosSlice";
 import vendasSlice from "./redux/vendasSlice";
-import ComprarSlice from "./redux/ComprarSlice";
+import ComprarSlice from "./redux/comprarSlice";
 
 const store = configureStore({
   reducer: {
