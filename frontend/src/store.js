4ed9fdec3486b35f8c0025749ce91e1fb62ca96b
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2021-12-29 12:56:14 -0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import eventosSlice from "./redux/eventosSlice";
 import vendasSlice from "./redux/vendasSlice";
 import ComprarSlice from "./redux/comprarSlice";
+import ingressos: fom "./redux/ingressosSlice";
 
 const store = configureStore({
   reducer: {
