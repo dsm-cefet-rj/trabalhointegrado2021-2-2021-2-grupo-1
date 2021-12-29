5de251eba4f8edb7ad2a5e97e883fb53834de174
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-29 18:30:02 -0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import eventosSlice from "./redux/eventosSlice";
 import vendasSlice from "./redux/vendasSlice";
-import ComprarSlice from "./redux/comprarSlice";
+import comprarSlice from "./redux/comprarSlice";
 import ingressosSlice from "./redux/ingressosSlice";
 
 const store = configureStore({
