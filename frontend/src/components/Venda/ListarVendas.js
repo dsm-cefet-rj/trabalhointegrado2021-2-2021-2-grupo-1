90e57ed15a6a3813289b6333b6c571c296b8b462
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-29 14:24:42 -0300

    Update ListarVendas.js

--- frontend/src/components/Venda/ListarVendas.js
+++ frontend/src/components/Venda/ListarVendas.js
@@ -1,21 +1,26 @@
+import { useSelector } from "react-redux";
+
 import Cabecalho from "../Cabecalho/Cabecalho";
 import Venda from "./Venda";
 
-function ListaVendas({vendas}) {
-  
+function ListarVendas() {
+  const eventos = useSelector((state) => state.vendas);
+
   return (
     <>
-      <Cabecalho usuario = {"empresa"} />
-      <main className = "centralizar-xy centralizar-y">
-        <h2 className = "subtitulo">Minhas Vendas</h2>
-        <div className = "listagem-container-empresa">
-          {vendas.map((venda) => (
-            <Venda venda={venda} />
-          ))}
+      <Cabecalho usuario={"empresa"} />
+      <main className="centralizar-xy centralizar-y">
+        <h2 className="subtitulo">Minhas Vendas</h2>
+        <div className="listagem-container-empresa">
+          {eventos.length > 0 ? (
+            vendas.map((evento) => <Venda Venda={venda} key={venda.id} />)
+          ) : (
+            <h2 className="subtitulo">Nenhuma Venda Cadastrada</h2>
+          )}
         </div>
       </main>
     </>
   );
 }
 
-export default ListaVendas;
+export default ListarVendas;
