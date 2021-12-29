7348696231ae83871ce1bd9d1042fecdea1e3c37
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2021-12-29 14:22:40 -0300

    Update EditarVenda.js

--- frontend/src/components/Venda/EditarVenda.js
+++ frontend/src/components/Venda/EditarVenda.js
@@ -1,33 +1,107 @@
 import Cabecalho from "../Cabecalho/Cabecalho";
 import Botoes from "../Botoes/Botoes";
 
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+
+import { editVenda, deleteVenda } from "../../redux/vendasSlice";
+
+import Cabecalho from "../Cabecalho/Cabecalho";
+
 function EditarVenda() {
-  const botoes = [
-    {
-      nome: "Editar",
-      url: "/editar-venda",
-    },
-    {
-      nome: "Remover",
-      url: "/",
-    },
-  ];
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const evento = useSelector((state) => state.eventos.find((e) => e.id === id));
+
+  const [vendaEditada, setVendaEditada] = useState({
+    nomeEvento: venda.nome,
+    idIngresso: venda.id,
+    genero: venda.genero,
+    valor: venda.valor,
+    quantidade: venda.quantidade,
+  });
+
+  function checaMudanca(e) {
+    setEventoEditado({
+      ...eventoEditado,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function checaEnvio(e) {
+    dispatch(editVenda(vendaEditada));
+    e.preventDefault();
+    navigate("/empresa/vendas");
+  }
+
+  function deletaVenda(e) {
+    dispatch(deleteVenda(venda.id));
+    e.preventDefault();
+    navigate("/empresa/vendas");
+  }
 
   return (
     <>
-      <Cabecalho usuario = {"empresa"} />
-      <main className = "centralizar-xy centralizar-y">
-        <h2 className = "subtitulo">Editar Venda</h2>
-        <form className = "formulario">
+      <Cabecalho usuario={"empresa"} />
+      <main className="centralizar-xy centralizar-y">
+        <h2 className="subtitulo">Criar Venda</h2>
+        <form className="formulario" onSubmit={checaEnvio}>
+          <label>Nome do Evento</label>
+          <input
+            type="text"
+            name="nomeEvento"
+            className="input-box"
+            placeholder="Rock in Rio"
+            onChange={checaMudanca}
+          />
+          <label>
+            Identificação do Ingresso
+            <select
+              className="input-box"
+              name="idIngresso"
+              onChange={checaMudanca}
+            >
+              <option>Rock in Rio 2021 - Dia 5</option>
+              <option>Rock in Rio 2021 - Dia 7</option>
+            </select>
+          </label>
+          <label>
+            Genero
+            <select className="input-box" name="genero" onChange={checaMudanca}>
+              <option>Esportes</option>
+              <option>Shows</option>
+              <option>Família</option>
+            </select>
+          </label>
           <label>
             Valor do Ingresso
-            <input type="number" placeholder="100" className="input-box" />
+            <input
+              type="number"
+              placeholder="100"
+              className="input-box"
+              name="valor"
+              onChange={checaMudanca}
+            />
           </label>
           <label>
             Quantidade de Ingressos
-            <input type="number" placeholder="2" className="input-box" />
+            <input
+              type="number"
+              placeholder="2"
+              className="input-box"
+              name="quantidade"
+              onChange={checaMudanca}
+            />
           </label>
-          <Botoes botoes = {botoes} />
+          <div className="botoes-container">
+            <input
+              type="submit"
+              value="Criar"
+              className="botao botao-sucesso"
+            />
+          </div>
         </form>
       </main>
     </>
