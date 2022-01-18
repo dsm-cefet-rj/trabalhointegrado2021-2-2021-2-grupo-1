d0faee9de9a4860fb3851ea75fb6010649615f73
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2022-01-18 13:24:14 -0300

    Update vendasSlice.js

--- frontend/src/redux/vendasSlice.js
+++ frontend/src/redux/vendasSlice.js
@@ -1,40 +1,101 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
 
-const initialState = [];
+import { httpDelete, httpGet, httpPut, httpPost } from "../utils";
 
-export const fetchVendas = createAsyncThunk("vendas/fetchVendas", async () => {
-  return await (await fetch("http://localhost:3001/vendas")).json();
+const vendasAdapter = createEntityAdapter();
+
+const initialState = vendasAdapter.getInitialState({
+  status: "not_loaded",
 });
+
+export const fetchVendass = createAsyncThunk(
+  "vendas/fetchVendas",
+  async () => {
+    return await httpGet("http://localhost:3001/vendas");
+  }
+);
+
+export const addVenda = createAsyncThunk(
+  "vendas/addVenda",
+  async (projeto) => {
+    return await httpPost("http://localhost:3001/vendas", projeto);
+  }
+);
+
+export const deleteVenda = createAsyncThunk(
+  "vendas/deleteVenda",
+  async (id) => {
+    await httpDelete(`http://localhost:3001/vendas/${id}`);
+    return id;
+  }
+);
+
+export const updateVenda = createAsyncThunk(
+  "vendas/updateVenda",
+  async (venda) => {
+    return await httpPut(`http://localhost:3001/vendas/${venda.id}`, venda);
+  }
+);
 
 const vendasSlice = createSlice({
   name: "vendas",
   initialState,
-  reducers: {
-    addVenda: (state, action) => [...state, action.payload],
-    vendaRealizada: (state, action) => {
-      return state.map((e) => {
-        if (e.id === action.payload) {
-          return { ...e, quantidade: e.quantidade - 1 };
-        }
-        return e;
-      });
-    },
-    editVenda: (state, action) => {
-      return state.map((e) =>
-        e.id === action.payload.id ? { ...e, ...action.payload } : e
-      );
-    },
-    deleteVenda: (state, action) => {
-      const index = state.findIndex((e) => e.id === action.payload);
-
-      state.splice(index, 1);
-    },
-  },
   extraReducers: {
-    [fetchVendas.fulfilled]: (state, action) => action.payload,
+    [fetchVendas.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      vendasAdapter.setAll(state, action.payload);
+    },
+    [fetchVendas.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [fetchVendas.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao buscar projetos: " + action.error.message;
+    },
+    [addVenda.fulfilled]: (state, action) => {
+      state.status = "saved";
+      vendasAdapter.addOne(state, action.payload);
+    },
+    [addVenda.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [addVenda.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao adicionar projeto: " + action.error.message;
+    },
+    [updateVenda.fulfilled]: (state, action) => {
+      state.status = "saved";
+      vendasAdapter.upsertOne(state, action.payload);
+    },
+    [updateVenda.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [updateVenda.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao editar projeto: " + action.error.message;
+    },
+    [deleteVenda.fulfilled]: (state, action) => {
+      state.status = "saved";
+      vendasAdapter.removeOne(state, action.payload);
+    },
+    [deleteVenda.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [deleteVenda.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao deletar projeto: " + action.error.message;
+    },
   },
 });
 
-export const { addVenda, vendaRealizada, editVenda, deleteVenda } =
-  vendasSlice.actions;
+export const {
+  selectAll: selectAllVendas,
+  selectById: selectVendaById,
+  selectIds: selectVendassIds,
+} = vendasAdapter.getSelectors((state) => state.vendas);
+
 export default vendasSlice.reducer;
