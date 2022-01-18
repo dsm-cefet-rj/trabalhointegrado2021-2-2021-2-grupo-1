5110aa08dd51c988deab38c1e2833d8769ef2a65
lucasmuguet <79877581+lucasmuguet@users.noreply.github.com>
2022-01-17 22:53:18 -0300

    Update comprarSlice.js

--- frontend/src/redux/comprarSlice.js
+++ frontend/src/redux/comprarSlice.js
@@ -1,4 +1,8 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+
+import { httpDelete, httpGet, httpPut, httpPost } from "../utils";
+
+const comprasAdapter = createEntityAdapter();
 
 const initialState = {
   meuCarrinho: [],
@@ -9,6 +13,29 @@ export const fetchComprar = createAsyncThunk(
   "comprar/fetchComprar",
   async () => {
     return await (await fetch("http://localhost:3001/compras")).json();
+        return await httpGet("http://localhost:3001/compras");
+  }
+);
+
+export const addCompra = createAsyncThunk(
+  "compras/addCompra",
+  async (projeto) => {
+    return await httpPost("http://localhost:3001/compras", projeto);
+  }
+);
+
+export const deleteCompra = createAsyncThunk(
+  "Compras/deleteCompra",
+  async (id) => {
+    await httpDelete(`http://localhost:3001/compras/${id}`);
+    return id;
+  }
+);
+
+export const updateCompra = createAsyncThunk(
+  "eventos/updateCompra",
+  async (compras) => {
+    return await httpPut(`http://localhost:3001/compras/${ingresso.id}`, ingresso);
   }
 );
 
@@ -33,42 +60,62 @@ const comprarSlice = createSlice({
         meuCarrinho: [],
       };
     },
-    addCompra: (state, action) => {
-      return {
-        ...state,
-        compras: [...state.compras, action.payload],
-      };
+    extraReducers: {
+    [fetchMeuCarrinho.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      comprasAdapter.setAll(state, action.payload);
+      
     },
-    editCompra: (state, action) => {
-      return {
-        ...state,
-        compras: state.compras.map((e) =>
-          e.id === action.payload.id ? { ...e, ...action.payload } : e
-        ),
-      };
+     [fetchCompras.pending]: (state, action) => {
+      state.status = "loading";
     },
-    deleteCompra: (state, action) => {
-      const index = state.compras.findIndex((e) => e.id === action.payload);
-
-      state.compras.splice(index, 1);
+    [fetchCompras.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao buscar projetos: " + action.error.message;
     },
-  },
-  extraReducers: {
-    [fetchComprar.fulfilled]: (state, action) => {
-      return {
-        ...state,
-        compras: action.payload,
-      };
+    [addCompra.fulfilled]: (state, action) => {
+      state.status = "saved";
+      comprasAdapter.addOne(state, action.payload);
     },
-  },
+    [addCompra.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [addCompra.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao adicionar projeto: " + action.error.message;
+    },
+    [updateCompra.fulfilled]: (state, action) => {
+      state.status = "saved";
+      comprasAdapter.upsertOne(state, action.payload);
+    },
+    [updateCompra.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [updateCompra.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao editar projeto: " + action.error.message;
+    },
+    [deleteCompra.fulfilled]: (state, action) => {
+      state.status = "saved";
+      comprasAdapter.removeOne(state, action.payload);
+    },
+    [deleteCompras.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [deleteCompra.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = "Falha ao deletar projeto: " + action.error.message;
 });
 
 export const {
   addMeuCarrinho,
   removeItemMeuCarrinho,
   removeTudoMeuCarrinho,
-  addCompra,
-  editCompra,
-  deleteCompra,
 } = comprarSlice.actions;
+    export const {
+  selectAll: selectAllCompras,
+  selectById: selectCompraById,
+  selectIds: selectComprasIds,
+} = comprasAdapter.getSelectors((state) => state.compras);
 export default comprarSlice.reducer;
+ 
