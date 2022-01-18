fdc848f19ac0014fbd3df087d58b9fc9033c9aaa
lucasmuguet <79877581+lucasmuguet@users.noreply.github.com>
2022-01-17 23:02:51 -0300

    Update ListarCompras.js

--- frontend/src/components/Comprar/ListarCompras.js
+++ frontend/src/components/Comprar/ListarCompras.js
@@ -5,26 +5,44 @@ import "../Ingresso/ingresso.css";
 import Cabecalho from "../Cabecalho/Cabecalho";
 import Ingresso from "../Ingresso/Pessoa/Ingresso";
 
+import loader from "../../../assets/loader.gif";
+import { selectAllCompras} from "../../../redux/comprasSlice";
+
 function ListarCompras() {
   const compras = useSelector((state) => state.comprar.compras);
+  
+  const compras = useSelector(selectAllCompras);
+  const { status, error } = useSelector((state) => {
+    return {
+      status: state.compras.status,
+      error: state.compras.error,
+    };
+  });
 
+  function mostreIngressosDeAcordoComOStatus() {
+    if (status === "loading") {
+      return <img src={loader} className="loader" />;
+    } else if (status === "loaded" || status === "saved") {
+      if (compras.length > 0) {
+        return compras.map((compra) => (
+          <Ingresso
+           vendaMeuCarrinhoOuCompra={"compra"}
+           key={compra.id} />
+        ));
+      } else {
+        return <h2 className="subtitulo">Você ainda não comprou nenhum ingresso!(</h2>;
+      }
+    } else if (status === "failed") {
+      return <p>Erro: {error}</p>;
+    }
+  }
   return (
     <>
       <Cabecalho usuario={"pessoa"} />
       <main className="centralizar-xy centralizar-y">
-        <h2 className="subtitulo">Meus Ingressos {compras.length}</h2>
+        <h2 className="subtitulo">Meus Ingressos</h2>
         <div className="ingresso-container">
-          {compras.length > 0 ? (
-            compras.map((compra) => (
-              <Ingresso
-                tipo={compra}
-                vendaMeuCarrinhoOuCompra={"compra"}
-                key={compra.id}
-              />
-            ))
-          ) : (
-            <p>Você ainda não comprou nenhum ingresso! :(</p>
-          )}
+        {mostreIngressosDeAcordoComOStatus()}
         </div>
       </main>
     </>
