7bc4a6c1b81bfc38bb6c98531c073756473158dc
Sérgio Luiz Batista de Souza Lemos Junior <79877844+SerchLemond@users.noreply.github.com>
2022-01-18 13:15:24 -0300

    Update ListarVendas.js

--- frontend/src/components/Venda/ListarVendas.js
+++ frontend/src/components/Venda/ListarVendas.js
@@ -2,21 +2,42 @@ import { useSelector } from "react-redux";
 
 import Cabecalho from "../Cabecalho/Cabecalho";
 import Venda from "./Venda";
+import loader from "../../assets/loader.gif";
+
+import { selectAllVendas } from "../../redux/vendasSlice";
 
 function ListarVendas() {
-  const vendas = useSelector((state) => state.vendas);
+  const vendas = useSelector(selectAllVendas);
+  const { status, error } = useSelector((state) => {
+    return {
+      status: state.eventos.status,
+      error: state.eventos.error,
+    };
+  });
+  
+function mostreVendasDeAcordoComOStatus() {
+  if (status === "loading") {
+    return <img src={loader} className="loader" />;
+  } else if (status === "loaded" || status === "saved") {
+    if (eventos.length > 0) {
+      return eventos.map((evento) => (
+        <Evento evento={evento} key={evento.id} />
+      ));
+    } else {
+      return <h2 className="subtitulo">Nenhum Evento Cadastrado :(</h2>;
+    }
+  } else if (status === "failed") {
+    return <p>Erro: {error}</p>;
+  }
+}
 
   return (
     <>
       <Cabecalho usuario={"empresa"} />
       <main className="centralizar-xy centralizar-y">
-        <h2 className="subtitulo">Minhas Vendas</h2>
+        <h2 className="subtitulo">Meus Eventos</h2>
         <div className="listagem-container-empresa">
-          {vendas.length > 0 ? (
-            vendas.map((venda) => <Venda venda={venda} key={venda.id} />)
-          ) : (
-            <h2 className="subtitulo">Nenhuma Venda Cadastrada</h2>
-          )}
+          {mostreVendasDeAcordoComOStatus()}
         </div>
       </main>
     </>
