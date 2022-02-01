8e44dcd2b955e25541bae21198e5e57649915aac
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2022-02-01 13:28:09 -0300

    Update IngressoSchema.js

--- frontend/src/components/Ingresso/Empresa/IngressoSchema.js
+++ frontend/src/components/Ingresso/Empresa/IngressoSchema.js
@@ -1,10 +1,10 @@
-import { string, object, number, setLocale } from "yup";
+import { string, object, setLocale } from "yup";
 import { ptForm } from "yup-locale-pt";
 
 setLocale(ptForm);
 
 const ingressoSchema = object({
-  id: number(),
+  id: string(),
   eventoId: string().required(),
   nome: string().required("É necessário ter um nome."),	
   horario: string().required("É necessário ter um horário.").matches(/^[0-9]{2}:[0-9]{2}$/, "O horário deve ser no formato hh:mm"),
