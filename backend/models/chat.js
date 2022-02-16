fb5bc8a30842e200549d715df66d6cfd94dec385
JoaoPedroNC <95342464+JoaoPedroNC@users.noreply.github.com>
2022-02-16 08:19:36 -0300

    Create chat.js

---
+++ backend/models/chat.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const chatSchema = new Schema({
+  tipo: { type: String, required: true },
+  username: { type: String, required: true },
+  password: String;
+  mensagem: String;
+  
+});
+
+chatSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+chatSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('chat', chatSchema);
