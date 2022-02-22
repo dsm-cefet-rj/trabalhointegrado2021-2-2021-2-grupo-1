17b47b29dc3b61efb2abe0764ac47090b39be5da
JoaoPedroNC <95342464+JoaoPedroNC@users.noreply.github.com>
2022-02-22 14:23:13 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,6 +32,11 @@ import EditarVenda from "./components/Venda/EditarVenda";
 import ListarVendas from "./components/Venda/ListarVendas";
 import { fetchVendas } from "./redux/vendasSlice";
 
+import Listarchat from "./components/Chat/ListarChat";
+import Editarchat from "./components/Chat/EditarChat";
+import Criarchat from "./components/Chat/CriarChat";
+import { fetchchat } from "./redux/chatSlice";
+
 function App() {
   const dispatch = useDispatch();
 
