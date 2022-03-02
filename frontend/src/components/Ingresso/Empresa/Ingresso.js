f0f5b9ca1246331e7266182ec8b19b9ed97ed041
IssamuTatch <61832372+IssamuTatch@users.noreply.github.com>
2022-03-01 21:08:48 -0300

    Update Ingresso.js

--- frontend/src/components/Ingresso/Empresa/Ingresso.js
+++ frontend/src/components/Ingresso/Empresa/Ingresso.js
@@ -1,6 +1,6 @@
 import Botao from "../../Botoes/Botoes";
 
-function Ingresso({ ingresso}) {
+function Ingresso({ ingresso }) {
   const botao = [
     {
       nome: "Editar",
@@ -11,6 +11,10 @@ function Ingresso({ ingresso}) {
   return (
     <div>
       <h2 className = "subtitulo" > {ingresso.nome} </h2>
+      <p>Evento: {ingresso.eventoID}</p>
+      <p>Horário: {ingresso.horario}</p>
+      <p>Data: {ingresso.data}</p>
+      <p>Dados Adicionais: {ingresso.descricao}</p>
       <Botao botoes = {botao} />
     </div>
   );
